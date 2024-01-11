0256f0b8b81bf87e584eb0548dfeee25cd6a6bd3
Nathan Owens <nmowens95@gmail.com>
2024-01-11 13:07:37 -0600

    Made a function that parses and grabs the api key

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,9 +1,25 @@
 package auth
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
 
 // GetAPIKey extracts an API Key from
 // the headers of an HTTP request
+// Example:
+// Authorization: ApiKey {insert apiKey here}
 func GetAPIKey(headers http.Header) (string, error) {
+	authHeader := headers.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("could not authenticate apiKey")
+	}
 
+	splitAuth := strings.Split(authHeader, " ")
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+		return "", errors.New("malformed authorization header")
+	}
+
+	return splitAuth[1], nil
 }
