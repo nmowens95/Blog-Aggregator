2ed2f4614e0aefe6db48754a30c1156c8cf5a571
Nathan Owens <nmowens95@gmail.com>
2024-01-11 13:06:50 -0600

    Made a get user function that takes an api key from the response header and matches it with a user to provide a response

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"http.github.com/nmowens95/Blog-Aggregator/internal/auth"
 	"http.github.com/nmowens95/Blog-Aggregator/internal/database"
 )
 
@@ -34,9 +35,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseUserToUser(user))
 }
 
 func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
 
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUserToUser(user))
 }
