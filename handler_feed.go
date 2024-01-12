64e4b1779886156e1bcc243e2488648a95d55b0e
Nathan Owens <nmowens95@gmail.com>
2024-01-12 12:28:49 -0600

    Created a handler to create a feed

---
+++ handler_feed.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"http.github.com/nmowens95/Blog-Aggregator/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("Could not parse JSON: %v", err))
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.Url,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could not create feed: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+}
