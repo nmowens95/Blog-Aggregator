bbe7c949890d18eacf72e35ae771e5e205306b65
Nathan Owens <nmowens95@gmail.com>
2024-01-15 13:24:08 -0600

    Tested our rss.go by calling fetchFeed

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -18,6 +19,12 @@ type apiConfig struct {
 }
 
 func main() {
+	feed, err := fetchFeed("https://wagslane.dev/index.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(feed)
+
 	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
