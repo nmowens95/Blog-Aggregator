2061207a98d9f52b1af21a179875863333f4108e
Nathan Owens <nmowens95@gmail.com>
2024-01-16 09:07:07 -0600

    Added logic to handle the null string for description regarding the post params. Wrote parsing for the published at param, alos we wrote that if there is a duplicate to return a duplicate key, otherwise it should log accordingly

--- scraper.go
+++ scraper.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"http.github.com/nmowens95/Blog-Aggregator/internal/database"
 )
 
@@ -42,18 +45,49 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 
 	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
 	if err != nil {
-		log.Println("Unable to mark feed", err)
+		log.Println("Unable to mark feed:", err)
 		return
 	}
 
 	rssFeed, err := fetchFeed(feed.Url)
 	if err != nil {
-		log.Println("Could not fetch feed", err)
+		log.Println("Could not fetch feed:", err)
 		return
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title)
+		// If value blank will be set to null, otherwise:
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		// Parsing published time, this doesn't handle all time formats
+		// As feeds are added may need to handle this edge case
+		pubAt, err := time.Parse(time.RFC1123Z, item.Pubdate)
+		if err != nil {
+			log.Printf("Couldn't parse date %v with err %v", item.Pubdate, err)
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Url:         item.Link,
+			Description: description,
+			PublishedAt: pubAt,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			// Only log if not duplicate, ex: we build twice
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("failed to create post", err)
+			return
+		}
 	}
 
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
